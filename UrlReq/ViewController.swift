8cd785daf44a51f81732e843a5e1fcb0217589a0
Koray Birand <info@koraybirand.com>
2018-10-22 21:57:11 +0300

    initial commit

--- UrlReq/ViewController.swift
+++ UrlReq/ViewController.swift
@@ -9,19 +9,65 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
+    
+    
+    let mainURL = "http://scouthy-test.com:8888/" + "admin/"
+    
+    
+    var koko = [[String:String]]() {
         didSet {
-        // Update the view, if already loaded.
+            //print(self.koko)
         }
     }
-
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startConnection(ps: "submit=1", file: "fillImages.php") { (json) in
+            self.koko = json
+            print(self.koko)
+        }
+        print(koko)
+        // Do any additional setup after loading the view.
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    func foo()
+{
+    
+    }
+    
+    func startConnection(ps: String, file: String, completion: @escaping ([[String:String]]) -> Void) {
+        let mainURL = "http://scouthy-test.com:8888/" + "admin/"
+        let url = URL(fileURLWithPath: mainURL).appendingPathComponent(file)
+        var request : URLRequest = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let postString = ps
+        request.httpBody = postString.data(using: .utf8)
+        
+        let dataTask = URLSession.shared.dataTask(with: request) {
+            data,response,error in
+            print("anything")
+            do {
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: []) as? [[String:Any]] {
+                    self.koko = jsonResult as! [[String:String]]
+              
+                    //Use GCD to invoke the completion handler on the main thread
+                    DispatchQueue.main.async() {
+                        completion(jsonResult as! [[String:String]])
+                    }
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+        dataTask.resume()
+    }
+    
+    
 }
 
