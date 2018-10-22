52a144e704140cf3fe7a6fc49c56df307c71903a
Koray Birand <info@koraybirand.com>
2018-10-22 21:15:29 +0300

    Initial Commit

---
+++ UrlReq/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UrlReq
+//
+//  Created by Koray Birand on 22.10.2018.
+//  Copyright © 2018 Koray Birand. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UrlReq/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UrlReq
+//
+//  Created by Koray Birand on 22.10.2018.
+//  Copyright © 2018 Koray Birand. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
